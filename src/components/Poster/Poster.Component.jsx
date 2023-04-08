dc05cc27166e31211d629d7cdaebed2e973b02ff
anisha-jadhav <jadhavanisha3033@gmail.com>
2023-04-08 20:46:36 +0530

    Completed Play page.

--- src/components/Poster/Poster.Component.jsx
+++ src/components/Poster/Poster.Component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Poster = (props) => {
+const MoviePoster = (props) => {
   return (
     <Link to={`/movie/${props.id}`}>
       <div className="flex flex-col items-start gap-2 px-1 md:px-3">
@@ -13,15 +13,53 @@ const Poster = (props) => {
           />
         </div>
         <h3
-          className={`text-lg font-semibold ${
-            props.isDark ? "text-white" : "text-dark"
-          }`}
+          className={`text-lg font-semibold ${props.isDark ? "text-white" : "text-dark"
+            }`}
         >
           {props.title}
         </h3>
       </div>
     </Link>
+    
   );
+};
+
+const PlayPoster = (props) => {
+  return (
+    <a href="https://in.bookmyshow.com/plays/disco-dancer-the-musical/ET00354671"
+      target="_blank"
+      rel= "noreferrer" >
+      <div className="flex flex-col items-start gap-2 px-1 md:px-3">
+        <div className="h-40 md:h-80">
+          <img
+            src={props.src}
+            alt="poster"
+            className="w-full h-full rounded-md object-cover object-center"
+          />
+        </div>
+        <h3
+          className={`text-lg font-semibold ${props.isDark ? "text-white" : "text-dark"
+            }`}
+        >
+          {props.title}
+        </h3>
+        <h5
+          className={`text-sm font ${props.isDark ? "text-white" : "text-dark"
+            }`}
+        >
+          {props.subtitle}
+        </h5>
+      </div>
+    </a>
+  );
+};
+
+const Poster = (props) => {
+  if(props.isPlay)
+  {
+    return <PlayPoster {...props} />;
+  }
+  return <MoviePoster {...props} />
 };
 
 export default Poster;
--- src/pages/Play.page.jsx
+++ src/pages/Play.page.jsx
@@ -1,8 +1,95 @@
 import React from "react";
 import DefaultlayoutHoc from "../layout/Default.layout";
 
+// components
+import Poster from "../components/Poster/Poster.Component";
+import PlayFilter from "../components/PlayFilters/PlayFilter.Components";
+
 const PlayPage = () => {
-  return <div>PlayPage</div>;
+  return (
+    <>
+      <div className="container mx-auto px-4 my-10">
+        <div className="w-full flex flex-col-reverse lg:flex-row-reverse gap-4">
+          <div className="lg:w-3/4 p-4 bg-white rounded">
+            <h2 className="text-2xl font-bold mb-4">Plays in Mumbai</h2>
+            <div className="flex flex-wrap">
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-ati-aani-sharti-lagu-0-2023-2-28-t-15-39-2.jpg"
+                  title="NIYAM V ATI LAGU
+
+"
+                  subtitle="Comedy, Drama | Marathi | 2hrs 30mins"
+                />
+              </div>
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/mobile/media-mobile-charchughi-0-2022-9-2-t-9-33-25.jpg"
+                  title="Charchaughi"
+                  subtitle="Drama | Marathi | 5yrs + | 2hrs 45mins"
+                />
+              </div>
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-the-sound-of-music-0-2023-4-7-t-15-15-39.jpg"
+                  title="The Sound of Music"
+                  subtitle="Broadway, Musical | English | 2hrs 45mins"
+                />
+              </div>
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-disco-dancer-the-musical-0-2023-4-5-t-6-7-29.jpg"
+                  title="Disco Dancer"
+                  subtitle="The Musical"
+                />
+              </div>
+
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-amar-photo-studio-0-2022-12-27-t-14-51-7.jpg"
+                  title="Amar Photo Studio
+
+"
+                  subtitle="Drama | Marathi | 2hrs 30mins"
+                />
+              </div>
+
+              <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+                <Poster
+                  isPlay={true}
+                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-albattya-galbattya-0-2022-9-7-t-12-7-0.jpg"
+                  title="Albattya Galbattya
+"
+                  subtitle="Fantasy | Marathi | 2hrs"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="lg:w-1/4 p-4 bg-white rounded">
+            <h2 className="text-2xl font-bold mb-4">Filters</h2>
+            <div>
+              <PlayFilter
+                title="Date"
+                tags={["Today", "Tomorrow", "This weekend"]}
+              />
+            </div>
+            <div>
+              <PlayFilter
+                title="Language"
+                tags={["English", "Hindi", "Tamil"]}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DefaultlayoutHoc(PlayPage);
